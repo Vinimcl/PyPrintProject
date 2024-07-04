01dfc18838e874a29b557c75c8a1efbcc9f3d772
Vinícius Maciel <141379362+Vinimcl@users.noreply.github.com>
2024-07-04 16:16:10 -0300

    Update Program.py
    commit 2

--- pyPrintProject/Program.py
+++ pyPrintProject/Program.py
@@ -1,34 +1,56 @@
+import schedule
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-import time
 from datetime import datetime
+import os
 
-# Caminho ChromeDriver
+def take_screenshots():
+
 webdriver_path = 'C:\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe'
 
-# Caminho certificado digital
-user_data_dir = 'C:\HERMES E CPF.pfx'
+# Diretório que salva os prints
+screenshot_folder = 'C:\\Imagens Domicilio\\'
 
-# URL do site
-url = 'https://domicilio-eletronico.pdpj.jus.br/comunicacoes;tipo=Comunicacao'
-url = 'https://open.spotify.com/'
-url = 'https://www.zappingcontadores.com.br/'
+if not os.path.exists(screenshot_folder):
+    os.makedirs(screenshot_folder)
+
+# Adiciona certificado digital
+cert_path = 'C:\\Users\\Zapping010\\Certificados\\HERMES E CPF.pfx'  # Substitua pelo caminho do seu certificado
+cert_password = '5012'
+
+# URLs dos sites
+urls = [
+    'https://domicilio-eletronico.pdpj.jus.br/comunicacoes;tipo=Comunicacao'
+]
 
 options = webdriver.ChromeOptions()
-options.add_argument('--headless')  # Para executar em modo headless (sem interface gráfica)
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+
+# Carrega o certificado digital
+options.add_argument(f'--ssl-key-store-type=pkcs12')
+options.add_argument(f'--ssl-key-store={cert_path}')
+options.add_argument(f'--ssl-key-store-password={cert_password}')
 
 service = Service(webdriver_path)
 driver = webdriver.Chrome(service=service, options=options)
 
 try:
-    driver.get(url)
+    for idx, url in enumerate(urls):
+        driver.get(url)
+        time.sleep(5)
 
-    time.sleep(5)
-
-    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    screenshot_path = f'screenshot_{timestamp}.png'
-    driver.save_screenshot(screenshot_path)
-    print(f'Screenshot salvo em: {screenshot_path}')
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        screenshot_path = os.path.join(screenshot_folder, f'screenshot_{idx + 1}_{timestamp}.png')
+        driver.save_screenshot(screenshot_path)
+        print(f'Screenshot de {url} salvo em: {screenshot_path}')
 
 finally:
     driver.quit()
+
+schedule.every().day.at("10:00").do(take_screenshots)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
